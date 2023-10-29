1baf7669dc1e278e76ddc2817528b543176480b7
Tonkel <w.tucker.cac@gmail.com>
2023-10-29 16:02:45 -0400

    deletes unnecessary dependency

--- Assets/index.js
+++ Assets/index.js
@@ -2,7 +2,6 @@
 const inquirer = require("inquirer");
 const MaxLengthInputPrompt = require("inquirer-maxlength-input-prompt");
 const fs = require("fs");
-const { error } = require("console");
 //register as a type of input
 inquirer.registerPrompt("maxlength-input", MaxLengthInputPrompt);
 
